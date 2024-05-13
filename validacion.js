e626eba9b376c6b02f668b6fbdcc46ef695c8f88
Marcos <marcos.adil123123@gmail.com>
2024-05-12 21:10:29 -0600

    casi final

--- validacion.js
+++ validacion.js
@@ -1,5 +1,15 @@
 //Haz tú validación en javascript acá
 
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener("scroll", function() {
+        var formacion = document.querySelector(".formacion");
+        var formacionPosition = formacion.getBoundingClientRect().top;
+        var screenPosition = window.innerHeight / 1.3;
 
-
-
+        if (formacionPosition < screenPosition) {
+            formacion.classList.add("visible");
+        } else {
+            formacion.classList.remove("visible");
+        }
+    });
+});
